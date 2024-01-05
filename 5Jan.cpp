98226c3ea99a263c4f028c8290a80db955a07fda
SChakraborty04 <120569942+SChakraborty04@users.noreply.github.com>
2024-01-05 17:48:01 +0530

    Create 5Jan.cpp

---
+++ 5Jan.cpp
@@ -0,0 +1,16 @@
+//Problem: https://www.geeksforgeeks.org/problems/count-possible-ways-to-construct-buildings5007/1
+//dp problem
+
+int TotalWays(int N)
+	{
+	    const long long mod=1e9+7;
+	    vector<vector<long long>>dp(N+1,vector<long long>(2,1));
+	    for(int i=N-1;i>-1;i--){
+	    dp[i][1]=dp[i+1][0];
+	    int make,nomake;
+	    make=dp[i+1][1];
+	    nomake=dp[i+1][0];
+	    dp[i][0]=(make+nomake)%mod;
+	    }
+	    return (dp[0][0]*dp[0][0])%mod;
+	}
